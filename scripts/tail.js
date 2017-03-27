4c9f62dd87e4cb20e3c3d735926ac4f80fafe5bc
zhanghedr <hezhang0929@gmail.com>
2017-03-27 01:01:19 -0500

    “update”

--- scripts/tail.js
+++ scripts/tail.js
@@ -1,17 +1,17 @@
 // Add a tail to every post from tail.md
-// Great for adding copyright info
+// Copyright
 
-var fs = require('fs');
+// var fs = require('fs');
 
-hexo.extend.filter.register('before_post_render', function(data) {
-    if(data.copyright == false) 
-    	return data;
+// hexo.extend.filter.register('before_post_render', function(data) {
+//     if(data.copyright == false) 
+//     	return data;
 
-    var file_content = fs.readFileSync('tail.md');
-    if(file_content) { // data.content.length > 50
-        data.content += '<br><br><hr>' + file_content;
-        var permalink = '\n本文链接：' + data.permalink;
-        data.content += permalink;
-    }
-    return data;
-});
+//     var file_content = fs.readFileSync('tail.md');
+//     if(file_content) { // data.content.length > 50
+//         data.content += '<br><br><hr>' + file_content;
+//         var permalink = '\n本文链接：' + data.permalink;
+//         data.content += permalink;
+//     }
+//     return data;
+// });
